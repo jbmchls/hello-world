77025624c46fc83792ce8315f35a1eb062f8cba9
jlbrowne <55000761+jlbrowne@users.noreply.github.com>
2019-09-17 17:03:42 -0400

    Add files via upload

---
+++ Plasmodium_falciparum.sh
@@ -0,0 +1,10 @@
+cd ~/
+echo 'when prompted for username, enter anonymous'
+echo 'use your email as the password'
+mkdir Plasmodium_falciparum && cd $_
+ftp ftp.ncbi.nlm.nih.gov
+# enter 'anonymous' as un and your email as pw
+passive && prompt
+cd ftp.ncbi.nlm.nih.gov/genomes/archive/old_genbank/Plasmodium_falciparum/CHR1
+mget AL844501.*
+bye
