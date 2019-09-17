e539858ffb08dad94f8589fa5409723af01765b2
jlbrowne <55000761+jlbrowne@users.noreply.github.com>
2019-09-17 16:04:11 -0400

    Update Plasmodium_falciparum.sh

--- Plasmodium_falciparum.sh
+++ Plasmodium_falciparum.sh
@@ -1,4 +1,3 @@
-!/bin/bash
 cd ~/
 echo 'when prompted for username, enter anonymous'
 echo 'use your email as the password'
@@ -6,6 +5,6 @@ mkdir Plasmodium_falciparum && cd $_
 ftp ftp.ncbi.nlm.nih.gov
 # enter 'anonymous' as un and your email as pw
 passive && prompt
-cd ./genomes/archive/old_genbank/Plasmodium_falciparum/CHR1
+cd ftp.ncbi.nlm.nih.gov/genomes/archive/old_genbank/Plasmodium_falciparum/CHR1
 mget AL844501.*
 bye
